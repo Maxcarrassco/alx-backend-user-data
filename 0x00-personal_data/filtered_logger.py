d99a046fdea54f5da59c0fc9d02c5d1e11ec1f97
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-27 13:57:40 +0000

    Implemented a custom format method that obfuscate the message of a log before returning it

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ALX SE Backend User Data Module."""
 import re
+import logging
 from typing import List
 
 
@@ -9,3 +10,22 @@ def filter_datum(fields: List[str], redaction: str,
     """Return the Obfuscated version of a message."""
     return re.sub(
         fr"({'|'.join(fields)})=[^{separator}]*", fr"\1={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the message while obfuscating it."""
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
