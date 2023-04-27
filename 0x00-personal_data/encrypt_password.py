6c6ae6e2f76a50bc4b31535dbdafc813ec6ea5bd
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-27 18:34:39 +0000

    Implemented a function that check if a plain password is equal to the hashed password pass

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,7 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Return the hash of a password."""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if the plain password is equivalent to the hashed password."""
+    return bcrypt.checkpw(password.encode(), hashed_password)
