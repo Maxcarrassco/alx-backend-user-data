f8b267b6d346ec80be3b2a5e593aafb4985ed545
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-27 12:07:13 +0000

    Modified filtered_logger.py to used the correct regex

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,5 +7,5 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str, msg: str, sep: str) -> str:
     """Return the Obfuscated version of a message."""
     for field in fields:
-        msg = re.sub(f'({field}=[^{sep}]*)', f'{field}={redaction}', msg)
+        msg = re.sub(fr'(?<={field}=)([^{sep}]*)', redaction, msg)
     return msg
