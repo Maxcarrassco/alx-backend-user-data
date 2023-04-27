e74dd6530a50485382a6879c3b3100aac4a0e4c2
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-27 12:17:06 +0000

    Make filtered_logger.py less than 5 lines long

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,6 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, msg: str, sep: str) -> str:
-    """Return the Obfuscated version of a message."""
     for field in fields:
         msg = re.sub(fr'(?<={field}=)([^{sep}]*)', redaction, msg)
     return msg
