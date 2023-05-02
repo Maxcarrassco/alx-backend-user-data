13e6f3688a6c7b06eee06f04ee02996598000f78
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-02 22:24:11 +0000

    Implement a method named 'extract_base64_authorization_header' in BasicAuth that return None if the header is not a valid Basic Auth Header or the Header value excluding 'Basic'

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,12 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Implement all basic auth fynctionalities."""
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Return base64 encoded Basic Auth authorization header value."""
+        if not authorization_header or type(authorization_header) is not str:
+            return None
+        if authorization_header[:6] != 'Basic ':
+            return None
+        return authorization_header[6:]
