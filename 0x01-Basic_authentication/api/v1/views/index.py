0f5262e86b3378f968696f79036092242edf0edd
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-02 20:04:21 +0000

    Fixed bug in task 1 solution

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized')
-def raise_unauthorized() -> None:
+@app_views.route('/unauthorized', methods=["GET"], strict_slashes=False)
+def raise_unauthorized() -> str:
     """Raise unauthorized exception."""
     abort(401)
