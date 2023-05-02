5bac3e3d8367843ec9979b4468d3ba44ccfb258b
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-02 22:52:09 +0000

    Implemented 'extract_user_credentials' in BasicAuth that extract the user email and password from the decoded base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ALX SE Backend Basic Auth Module."""
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -28,3 +29,15 @@ class BasicAuth(Auth):
         except Exception:
             return None
         return cred.decode('utf-8')
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """Return the user credential from the decoded base64 string."""
+        if not decoded_base64_authorization_header:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        cred = decoded_base64_authorization_header.split(':')
+        return cred[0], cred[1]
