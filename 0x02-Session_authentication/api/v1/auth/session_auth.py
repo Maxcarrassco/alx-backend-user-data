b5f01135b43df6e4e883eca0ac2ac477a2013368
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-04 12:35:30 +0000

    Implement a method called create_session that creates a new user session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """ALX SE Backend Custom Session Auth Module."""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """This module implement all functionalities of a session auth service."""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a new user session."""
+        if not user_id or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
