df2d23c9fb23d27d61d6a8d0749f382d39a853ab
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-04 15:57:55 +0000

    Override the current_user method of Auth in SessionAuth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 from api.v1.auth.auth import Auth
 import uuid
 from typing import Dict, Union
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -23,3 +24,13 @@ class SessionAuth(Auth):
         if not session_id or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> Union[User, None]:
+        """Return the current user from db using its id from the session"""
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return None
+        return User.get(user_id)
