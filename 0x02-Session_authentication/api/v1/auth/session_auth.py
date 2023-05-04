96169cf28aa01457f0920a88bd8ee791b0afe240
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-04 12:26:32 +0000

    Setup the session auth module

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""ALX SE Backend Custom Session Auth Module."""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """This module implement all functionalities of a session auth service."""
+    pass
