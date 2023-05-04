a67bda7bc8b6cf282b965d84e92c147de1556320
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-04 22:16:49 +0000

    Implemented file storage for storaging user session instead in-memory

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Session Auth Module."""
+from models.base import Base
+
+
+class UserSession(Base):
+    """File storage for user session."""
+
+    def __init__(self, *args: list, **kwargs: dict) -> None:
+        """Initialize UserSession."""
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
