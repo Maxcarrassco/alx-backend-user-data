742c0f28a6e6c993fe9f406ebb4e556d797571ac
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-04 14:39:55 +0000

    Implemented a method named user_id_for_session_id that returns the user id from session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,16 +2,24 @@
 """ALX SE Backend Custom Session Auth Module."""
 from api.v1.auth.auth import Auth
 import uuid
+from typing import Dict, Union
 
 
 class SessionAuth(Auth):
     """This module implement all functionalities of a session auth service."""
-    user_id_by_session_id = {}
+    user_id_by_session_id: Dict[str, str] = {}
 
-    def create_session(self, user_id: str = None) -> str:
+    def create_session(self, user_id: str = None) -> Union[str, None]:
         """Create a new user session."""
         if not user_id or type(user_id) is not str:
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(
+            self, session_id: str = None) -> Union[str, None]:
+        """Return the user id from session."""
+        if not session_id or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
