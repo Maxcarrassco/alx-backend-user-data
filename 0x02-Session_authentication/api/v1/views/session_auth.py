2f26ba280f5aebd194f26381e8bee075ef9fac4d
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-04 17:47:40 +0000

    Fixed pycodestyle warning

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -30,7 +30,8 @@ def login_user_view() -> str:
     return {'error': 'no user found for this email'}, 404
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route(
+        '/auth_session/logout', methods=['DELETE'], strict_slashes=False)
 def logout_user_view() -> str:
     """Delete current user session."""
     from api.v1.app import auth
