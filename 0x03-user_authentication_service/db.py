920507d424076dec184357a61d53b5b88a74048b
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-09 10:22:53 +0000

    Added type annotation to the add_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the db storage and return the user object."""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
