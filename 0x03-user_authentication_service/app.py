07a539573e2cfa895125f33e9a910fb97351434c
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-11 17:34:03 +0000

    Added a route for registering user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 """App Server Module Of User Service."""
-from flask import Flask
+from flask import Flask, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
 def index():
     """Return home page."""
     return {"message": "Bienvenue"}
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user():
+    """Register a new user."""
+    form = request.form
+    try:
+        user = AUTH.register_user(form.get('email'), form.get('password'))
+        return {'email': user.email, 'message': 'user created'}
+    except ValueError:
+        return {'message': 'email already registered'}
 
 
 if __name__ == '__main__':
