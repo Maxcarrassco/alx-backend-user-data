06cecb1b9152afa65f881840ec233460b47fa8e1
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-11 22:57:24 +0000

    Fix a typo in update_password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,6 +85,6 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password).decode('utf-8')
             self._db.update_user(
-                    user.id, hashed_passord=hashed_password, reset_token=None)
+                    user.id, hashed_password=hashed_password, reset_token=None)
         except NoResultFound:
             raise ValueError
