abfb40919c8df43c747e9d04e4c16d044ad19254
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-11 22:39:51 +0000

    Implemented the reset_password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,5 +66,18 @@ def get_me():
     return {'email': user.email}, 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """Return reset password token."""
+    email = request.form.get('email')
+    if not email:
+        abort(403)
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return {'email': email, 'reset_token': token}
+    except ValueError:
+        abort(403)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
